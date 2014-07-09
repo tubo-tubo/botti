ae829e96099b633e8a63904ddc10919bd010bedd
HAYASAKA_Ryosuke <hayasaka.ryosuke@gmail.com>
2014-07-09 22:17:03 +0900

    add gpsnavi

---
+++ lib/python/test_gpsnavi.py
@@ -0,0 +1,25 @@
+import gpsnavi
+import unittest
+import datetime
+import mox3
+
+
+class Testgpsnavi(unittest.TestCase):
+    lon = 139.649867
+    lat = 35.705385
+    goal = [lon, lat]
+    gps = gpsnavi.gpsparser(goal=goal)
+
+    def test_readgps(self):
+        self.gps.gpsupdate(debuggpsvalue='$GPGGA,085120.307,3541.1493,N,13945.3994,E,1,08,1.0,6.9,M,35.9,M,,0000*5E')
+        self.assertEqual(self.gps.lat(), 3541.1493)
+        self.assertEqual(self.gps.lon(), 13945.3994)
+        self.assertEqual(self.gps.latitude(), 35.68582166666667)
+        self.assertEqual(self.gps.longitude(), 139.75665666666666)
+        self.assertEqual(self.gps.timestamp(), datetime.time(8, 51, 20))
+        self.assertEqual(self.gps.sat_receivejudge(), True)
+        self.assertEqual(self.gps.altitude(), 6.9)
+        print(self.gps.goaldistance())
+        #print(self.gps.goalazimath())
+
+unittest.main()
