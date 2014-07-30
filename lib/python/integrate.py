b4cc395558b3f7de1168646b6b21fc41a40dbd73
tubo-tubo <1130131@stu.hus.ac.jp>
2014-07-30 18:49:06 +0900

    integrate.py

---
+++ lib/python/integrate.py
@@ -0,0 +1,16 @@
+import go_GPIO
+import go_f
+
+lon1 = 
+lat1 = 
+lon2 = 
+lat2 = 
+lon3 = 
+lat3 = 
+
+go_f.getdistance
+goal = []
+goal.sppend([lon1, lat1]) 
+goal.sppend([lon2, lat2])
+goal.sppend([lon3, lat3])
+go_GPIO.GPIO.turn()
