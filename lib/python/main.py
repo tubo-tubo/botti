5ac9103c5cd9c3d97f68500fb3de22e50fd12eb0
tubo-tubo <1130131@stu.hus.ac.jp>
2014-08-06 13:20:35 +0900

    統合プログラム

--- lib/python/main.py
+++ lib/python/main.py
@@ -1,1 +1,15 @@
 #!coding:utf-8
+
+import go_GPIO
+
+lon1 =
+lat1 =
+lon2 =
+lat2 =
+lon3 =
+lat3 =
+
+go_f.getdistance._goal = [[lon1,lat1]]
+checkpoint = [[lon2,lat2],[lon3,lat3]]
+go_f.getdistance._goal.extend(checkpoint)
+go_GPIO.GPIO.turn()
