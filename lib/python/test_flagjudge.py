10730f26396674099e679302287b9657ce57975f
HAYASAKA_Ryosuke <hayasaka.ryosuke@gmail.com>
2014-08-08 00:09:08 +0900

    captest

--- lib/python/test_flagjudge.py
+++ lib/python/test_flagjudge.py
@@ -9,15 +9,16 @@ class flagjudgeTest(unittest.TestCase):
     def setUp(self):
         self.cap = flagjudge.flagcapture()
 
-    #def testcapture(self):
-    #    with self.assertRaises(ValueError):
-    #        self.cap.capture()
-    #        self.imagename = self.cap.imagename
+    def testcapture(self):
+        with self.assertRaises(ValueError):
+            self.cap.capture()
+            self.imagename = self.cap.imagename
+            print(self.cap.judge())
 
-    def testjudge(self):
-        self.cap.imagename = os.path.dirname(__file__)+'/colorcone2.jpg'
-        print(self.cap.imagename)
-        print(self.cap.judge())
+    #def testjudge(self):
+    #    self.cap.imagename = os.path.dirname(__file__)+'/colorcone2.jpg'
+    #    print(self.cap.imagename)
+    #    print(self.cap.judge())
 
 
 if __name__ == '__main__':
