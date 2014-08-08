e6cbe1bc4689f62911b1f32a675a0affb409932f
HAYASAKA_Ryosuke <hayasaka.ryosuke@gmail.com>
2014-08-08 23:11:17 +0900

    change test_gpio

--- lib/python/test_gpio.py
+++ lib/python/test_gpio.py
@@ -1,12 +1,24 @@
 #!coding:utf-8
 
-import RPi.GPIO as IO
+import os
+import sys
 import time
+import unittest
+if os.getlogin() == 'pi':
+    import RPi.GPIO as IO
+else:
+    sys.path.append(os.path.dirname(__file__))
+    from RpiTest import GPIO as IO
 
-IO.setmode(IO.BCM)
-IO.setup(18, IO.OUT)
 
-IO.output(18, IO.HIGH)
-time.sleep(1)
-IO.output(18, IO.LOW)
-time.sleep(1)
+class gpiotest(unittest.TestCase):
+    def testIO(self):
+        IO.setmode(IO.BCM)
+        IO.setup(18, IO.OUT)
+        IO.output(18, IO.HIGH)
+        time.sleep(1)
+        IO.output(18, IO.LOW)
+        time.sleep(1)
+
+if __name__ == '__main__':
+    unittest.main()
