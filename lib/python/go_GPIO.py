7293b345e96dc9c7a31d7ce3bf93872d56a650c6
tubo-tubo <1130131@stu.hus.ac.jp>
2014-08-08 22:04:01 +0900

    go_GPIO.py

--- lib/python/go_GPIO.py
+++ lib/python/go_GPIO.py
@@ -50,6 +50,7 @@ class GPIO:  # GPIOをTurtleに
         self.IO.output(self.leftmotor[1], True)
         self.IO.output(self.rightmotor[0], True)
         self.IO.output(self.rightmotor[1], False)
+        self.time.sleep(0.01)
         logging.info("right")
 
     def y_coordinates(self):
@@ -68,6 +69,16 @@ class GPIO:  # GPIOをTurtleに
         else:
             return False
 
+    def angle(self):
+        if 0 <= self.gps.goalazimath < 90 or 270 < self.gps.goalazimath <= 360 and self.y_coordinates() < self.y_coord:    #1st quadrant , 4th quadrant , south direction
+                    self.right(self.gps.goalazimath)
+        elif 0 <= self.gps.goalazimath < 90 or 270 < self.gps.goalazimath <= 360 and self.y_coordinates() > self.y_coord:   #1nd quadrant , 4rd quadrant , north direction
+                    self.left(self.gps.goalazimath)
+        elif 90 <= self.gps.goalazimath <= 270 and self.y_coordinates() < self.y_coord:     #2nd quadrant , 3rd quadrant , south direction
+                    self.left(self.gps.goalazimath)
+        elif 90 <= self.gps.goalazimath <= 270 and self.y_coordinates() > self.y_coord:     #2nd quadrant , 3rd quadrant , north direction
+                    self.right(self.gps.goalszimath)
+
     def turn(self):
         while len(self.gps.goal) > 0:  # If "len" is 0,roba is stop.
             while self.gps.goaldistance > 30:
@@ -78,16 +89,7 @@ class GPIO:  # GPIOをTurtleに
                         break
 
                 self.time.sleep(0.01)
-
-                if 0 <= self.gps.goalazimath < 90 or 270 < self.gps.goalazimath <= 360 and self.y_coordinates() < self.y_coord:    #1st quadrant , 4th quadrant , south direction
-                    self.right(self.gps.goalazimath)
-                elif 0 <= self.gps.goalazimath < 90 or 270 < self.gps.goalazimath <= 360 and self.y_coordinates() > self.y_coord:   #1nd quadrant , 4rd quadrant , north direction
-                    self.left(self.gps.goalazimath)
-                elif 90 <= self.gps.goalazimath <= 270 and self.y_coordinates() < self.y_coord:     #2nd quadrant , 3rd quadrant , south direction
-                    self.left(self.gps.goalazimath)
-                elif 90 <= self.gps.goalazimath <= 270 and self.y_coordinates() > self.y_coord:     #2nd quadrant , 3rd quadrant , north direction
-                    self.right(self.gps.goalszimath)
-
+                self.angle()
                 self.time.sleep(0.01)
                 self.forward(self.gps.goaldistance())
                 self.time.sleep(0.01)
