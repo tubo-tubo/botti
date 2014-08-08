eb852c6e9a8bffc1d66aa0209142680fe9eb45b6
tubo-tubo <1130131@stu.hus.ac.jp>
2014-08-08 16:30:32 +0900

    main.py

--- lib/python/main.py
+++ lib/python/main.py
@@ -1,15 +1,33 @@
 #!coding:utf-8
 
 import go_GPIO
+import numpy
+import time
 
-lon1 =
-lat1 =
-lon2 =
-lat2 =
-lon3 =
-lat3 =
+gps = gpsnavi.gpsparser(goal=goal)
+groundlat = gps.altitude()	#現地で計測する予定
+altave = []
+passer = []
 
-go_f.getdistance._goal = [[lon1,lat1]]
-checkpoint = [[lon2,lat2],[lon3,lat3]]
-go_f.getdistance._goal.extend(checkpoint)
-go_GPIO.GPIO.turn()
+if gps.altitude >= groundlat + 50:
+	passer.append("high")
+
+while gps.altitude() <= groundlat + 30 and passer[0] = "high" :
+	time.sleep(0.5)
+	altave.append(gps.altitude())
+
+	if groundlat - 10 <= numpy.average(altave) <= groundlat + 10 :
+		time.sleep(30)
+		go_GPIO.forward(20)
+
+		lon = 141.24322166666667
+		lat = 43.123041666666666
+		lon1 = 139.649867
+		lat1 = 35.705385
+		#lon2 =
+		#lat2 =
+		goal = [[lon,lat]]
+		check = [[lon1,lat1]]
+		goal.extend(check)
+
+		go_GPIO.GPIO.turn()
