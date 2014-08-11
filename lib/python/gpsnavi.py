f6b5318208cf968a4e775619e7f6a3f06c9548a5
HAYASAKA.Ryosuke <hayasaka.ryosuke@gmail.com>
2014-08-11 18:50:03 +0900

    randingのテスト用データとそのmainテストの修正

--- lib/python/gpsnavi.py
+++ lib/python/gpsnavi.py
@@ -3,6 +3,7 @@
 import serial
 import math
 import pynmea2
+import pynmea2
 from pyproj import Geod
 import logging
 
@@ -46,15 +47,17 @@ class gpsparser(object):
         return float(self.gpsdata.longitude)
 
     def sat_receivejudge(self):
-        if int(self.gpsdata.num_sats) >= 4:
+        if int(self.gpsdata.num_sats) >= 3:
             logging.info("OKGpsnum:"+str(self.gpsdata.num_sats))
+            print("OKGpsnum:"+str(self.gpsdata.num_sats))
             return True
         else:
             logging.info("NoGpsnum:"+str(self.gpsdata.num_sats))
+            print("NoGpsnum:"+str(self.gpsdata.num_sats))
             return False
 
     def altitude(self):
-        return float(self.gpsdata.altitude)
+        return float(self.gpsdata.geo_sep)
 
     def goalcalc(self):
         nowpos = [self.longitude(), self.latitude()]
