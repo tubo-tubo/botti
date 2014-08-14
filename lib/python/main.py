2e5b92b1f27d096d17bab88caeb10b82e17d3c63
tubo-tubo <1130131@stu.hus.ac.jp>
2014-08-14 21:28:45 +0900

    main.py

--- lib/python/main.py
+++ lib/python/main.py
@@ -9,7 +9,7 @@ import datetime
 
 class Main(object):
 
-    def __init__(self, groundalt=0, gpsport=None, gpsbaudrate=9600, maxalt=80, goal=[[141.24966333333333, 43.13460166666667], [141.24322166666667, 43.123041666666666]], ratio=96, rate=0.0164):
+    def __init__(self, groundalt=0, gpsport=None, gpsbaudrate=9600, maxalt=80, goal=[[139.9873791954023, 40.14224106321837], ratio=96.0, rate=0.0164):
         logging.basicConfig(format='%(asctime)s %(message)s', filename="botti"+str(time.strftime('%H-%M-%S', datetime.datetime.now().timetuple()))+'.log', level=logging.INFO)
         self.gpsdebugvalue = None
         self.maxalt = maxalt
