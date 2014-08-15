e1ae3d9be262a32ce74fc576781130ddff6dffdb
HAYASAKA.Ryosuke <hayasaka.ryosuke@gmail.com>
2014-08-16 01:58:22 +0900

    モータ制御のチェック用スクリプト

---
+++ lib/python/test_motorcontrol.py
@@ -0,0 +1,16 @@
+import go_GPIO
+
+go = go_GPIO()
+while True:
+    print('left:0,right:1,forward:2,back:3,stop:4')
+    i = int(input().split()[0])
+    if i == 0:
+        go.left(30)
+    elif i== 1:
+        go.right(30)
+    elif i== 2:
+        go.forward(30)
+    elif i== 3:
+        go.back(30)
+    elif i==4:
+        go.stop()
